31cdad7a27837b9beca9806b4f0252183cc2aaeb
koundalamit <koundalamit5991@outlook.com>
2023-11-29 17:38:00 +0530

    first commit

---
+++ src/main/java/com/syno/word/SynonameWordApplication.java
@@ -0,0 +1,13 @@
+package com.syno.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynonameWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynonameWordApplication.class, args);
+	}
+
+}
